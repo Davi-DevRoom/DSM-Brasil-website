7f50efb2bf76a41fa3a7a8063a390d991653daed
Davi Luiz <aloha.devdl@gmail.com>
2024-08-07 14:05:25 -0400

    update seção de produtos

--- JS/script.js
+++ JS/script.js
@@ -40,3 +40,23 @@ const mobileNavbar = new MobileNavbar(
     ".nav-list li",
 );
 mobileNavbar.init();
+
+
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+
+document.getElementById('radio1').checked =  true 
+
+setInterval(() => {
+    proxImg()
+}, 6000)
+
+function proxImg(){
+    cont ++
+
+    if(cont > 4){
+        cont = 1
+    }
+    document.getElementById('radio'+cont).checked = true 
+}
